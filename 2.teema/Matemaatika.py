c34fbeb5cc8bd1e6d85e7e72350a04c7437dc872
Sirli Anier <sirli.anier.001@khk.ee>
2021-02-11 11:46:15 +0000

    Klassi ja eksemplaari loomise katse

---
+++ 2.teema/Matemaatika.py
@@ -0,0 +1,5 @@
+class Matemaatika():
+    number = 5
+
+    def liitmine(self, arv):
+       print(self.number + arv)
---
+++ 2.teema/test.py
@@ -0,0 +1,8 @@
+from Matemaatika import A
+# from file import klass
+a = Matemaatika()
+print(a)
+print(a.number)
+a.number = 8
+print(a.number)
+a.liitmine(4)
