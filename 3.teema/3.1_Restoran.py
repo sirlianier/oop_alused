b8bbae2dfe6a5c1ee8a56dd39e0a45de330a939d
Sirli Anier <sirli.anier.001@khk.ee>
2021-02-16 07:28:05 +0000

    Restoran ülesanne

---
+++ 3.teema/3.1_Restoran.py
@@ -0,0 +1,11 @@
+class Restoran():
+    def __init__(self, nimi, soogi_tyyp):
+    self.restorani_nimi = nimi
+    self.soogi_tyyp = tyyp
+    print("Restoran nimega {} ".format(self.restorani_nimi)
+
+    def restorani_kirjeldus(self):
+        print("Restoran " + str(self.restorani_nimi) + ", söögi tüüp " + str(self.soogi_tyyp))
+
+    def ava_restoran(self):
+        print("Restoran on avatud")
